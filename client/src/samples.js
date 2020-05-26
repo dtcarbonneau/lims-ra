f37a5c0038cd23b8d07b8660bf52e5783d206208
Carbonneau <David.Carbonneau@aecom.com>
2020-05-26 14:36:36 -0400

    samples.js

--- client/src/samples.js
+++ client/src/samples.js
@@ -4,6 +4,7 @@ import { Filter, List, Datagrid, TextField, EmailField, ReferenceField,
         ReferenceInput, SelectInput, NumberField, DateField, EditButton,
         Edit, SimpleForm, TextInput, DateInput, NumberInput, BulkDeleteButton, Create} from 'react-admin';
 import ShipSampButton from './ShipSampButton';
+import InsertManyButton from './InsertMany';
 
 const SamplesFilter = (props) => (
     <Filter {...props}>
@@ -22,6 +23,31 @@ const SamplesBulkActionButtons = props => (
         {/* default bulk delete action */}
         <BulkDeleteButton {...props} />
     </Fragment>
+);
+
+export const SampleCreate = props => (
+    // <Create {...props}>
+    <Create {...props}>
+      <Fragment>
+        <InsertManyButton label="Insert Samples" {...props} />
+      </Fragment>
+    </Create>
+        // <SimpleForm>
+        //   <ReferenceInput source="u_id" reference="users" label="User">
+        //     <SelectInput optionText="last_name" />
+        //   </ReferenceInput>
+        //   <ReferenceInput source="ss_id" reference="s_status" label="Status">
+        //     <SelectInput optionText="ss_name" />
+        //   </ReferenceInput>
+        //   <ReferenceInput source="p_id" reference="projects" label="Projects">
+        //     <SelectInput optionText="p_name" />
+        //   </ReferenceInput>
+        //   <TextInput source="loc" label="Location"/>
+        //   <DateInput source="date_cryo" label="Cryo Date" />
+        //   <DateInput source="date_exp" label="Expiration Date"/>
+        // </SimpleForm>
+    // </Create>
+
 );
 
 export const SampleEdit = props => (
@@ -61,24 +87,24 @@ export const SampleList = props => (
     </List>
 );
 
-export const SampleCreate = props => (
-    <Create {...props}>
-        <SimpleForm>
-          <ReferenceInput source="u_id" reference="users" label="User">
-            <SelectInput optionText="last_name" />
-          </ReferenceInput>
-          <ReferenceInput source="ss_id" reference="s_status" label="Status">
-            <SelectInput optionText="ss_name" />
-          </ReferenceInput>
-          <ReferenceInput source="p_id" reference="projects" label="Projects">
-            <SelectInput optionText="p_name" />
-          </ReferenceInput>
-          <TextInput source="loc" label="Location"/>
-          <DateInput source="date_cryo" label="Cryo Date" />
-          <DateInput source="date_exp" label="Expiration Date"/>
-        </SimpleForm>
-    </Create>
-);
+// export const SampleCreate = props => (
+//     <Create {...props}>
+//         <SimpleForm>
+//           <ReferenceInput source="u_id" reference="users" label="User">
+//             <SelectInput optionText="last_name" />
+//           </ReferenceInput>
+//           <ReferenceInput source="ss_id" reference="s_status" label="Status">
+//             <SelectInput optionText="ss_name" />
+//           </ReferenceInput>
+//           <ReferenceInput source="p_id" reference="projects" label="Projects">
+//             <SelectInput optionText="p_name" />
+//           </ReferenceInput>
+//           <TextInput source="loc" label="Location"/>
+//           <DateInput source="date_cryo" label="Cryo Date" />
+//           <DateInput source="date_exp" label="Expiration Date"/>
+//         </SimpleForm>
+//     </Create>
+// );
 
 // export const SampleEdit = props => (
 //     <Edit {...props}>
